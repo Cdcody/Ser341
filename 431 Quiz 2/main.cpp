a35ef343fbbc487cbadc31e1616536efd3cbe03b
Justin Vredevoogd <justinvred@gmail.com>
2018-09-06 18:05:46 -0700

    Skybox fixes, right click menu, rotating box, improved movement in the works

--- 431 Quiz 2/main.cpp
+++ 431 Quiz 2/main.cpp
@@ -11,12 +11,41 @@
 #include "render.h"
 #include "controls.h"
 
-#define terrainSamples 100
-
-// global
+#define terrainSamples 100 
+ // global
 Mesh *floorPlane, *cube1, *cube2, *cube3, *skybox, *mountains;
 GLuint display1, display2, display3, display4, display5, display6;
 GLuint textures[6];
+int turning = 0, o = 0;
+int FAST = 0;
+
+
+
+// menuListener
+void menuListener(int option) {
+	switch (option) {
+	case 1:
+		FAST = 1; break;
+	case 2:
+		FAST = 0; break;
+	}
+	glutPostRedisplay();
+}
+
+// create menu
+void createMenus() {
+
+	// add entries to speed
+	int menuA = glutCreateMenu(menuListener);
+	glutAddMenuEntry("faster", 1);
+	glutAddMenuEntry("normal", 2);
+	// create main menu
+	int menu = glutCreateMenu(menuListener);
+	glutAddSubMenu("Speed", menuA);
+	// attach the menu to the right button
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+}
+
 
 // init
 void init() {
@@ -27,14 +56,13 @@ void init() {
 	cube3 = createCube();
 	skybox = createSkyBox(6000);
 
-
 	ImprovedNoise* noise = new ImprovedNoise();
 
 	//initializes array for heightmap
 	double** points = new double*[terrainSamples];
 	for (int i = 0; i < terrainSamples; i++) {
 		points[i] = new double[terrainSamples];
-		for(int j = 0; j < terrainSamples; j++) {
+		for (int j = 0; j < terrainSamples; j++) {
 			points[i][j] = 0;
 		}
 	}
@@ -46,7 +74,9 @@ void init() {
 		}
 	}
 	mountains = createTerrain(100, points);
-	
+
+
+
 	// normals
 	calculateNormalPerFace(floorPlane);
 	calculateNormalPerVertex(floorPlane);
@@ -63,7 +93,7 @@ void init() {
 	calculateNormalPerFace(skybox);
 	calculateNormalPerVertex(skybox);
 
-	
+
 	// textures
 	loadBMP_custom(textures, "_BMP_files/brick.bmp", 0);
 	loadBMP_custom(textures, "_BMP_files/oldbox.bmp", 1);
@@ -80,16 +110,16 @@ void init() {
 	display4 = meshToDisplayList(cube3, 4, textures[3]);
 	display5 = meshToDisplayList(skybox, 5, textures[4]);
 	display6 = meshToDisplayList(mountains, 6, textures[4]);
-	
+
 	// configuration
 	glShadeModel(GL_SMOOTH);
 	glEnable(GL_DEPTH_TEST);
-	
+
 	// light
 	glEnable(GL_LIGHT0);
 	glEnable(GL_LIGHTING);
-	GLfloat light_ambient[]  = { 0.5, 0.5, 0.5, 1.0 };
-	GLfloat light_diffuse[]  = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat light_ambient[] = { 0.5, 0.5, 0.5, 1.0 };
+	GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
 	GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
 	GLfloat light_position[] = { 0.0, 0.0, 1.0, 0.0 };
 	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
@@ -129,7 +159,7 @@ void display(void) {
 	// lookAt
 	// gluLookAt(0.0f, 40.0f, 320.0,	0.0f, 1.0f, -1.0f,		0.0f, 1.0f, 0.0f);
 
-	gluLookAt(camera_x, camera_y, camera_z, camera_viewing_x, camera_viewing_y, camera_viewing_z,		0.0f, 1.0f, 0.0f);
+	gluLookAt(camera_x, camera_y, camera_z, camera_viewing_x, camera_viewing_y, camera_viewing_z, 0.0f, 1.0f, 0.0f);
 	// camera
 	//glScalef(scale, scale, scale);
 	//glRotatef(x_angle, 1.0f, 0.0f, 0.0f);
@@ -137,10 +167,10 @@ void display(void) {
 	//glTranslatef(0.0f, 0.0f, 0.0f);
 
 	//plane
-	glPushMatrix();
-	glTranslatef(-1000, 200, -1000);
-	glCallList(display1);
-	glPopMatrix();
+	//glPushMatrix();
+	//glTranslatef(-1000, 200, -1000);
+	//glCallList(display1);
+	//glPopMatrix();
 	// box 1
 	glPushMatrix();
 	glTranslatef(0, 300, 0);
@@ -149,6 +179,10 @@ void display(void) {
 	// box 2
 	glPushMatrix();
 	glTranslatef(200, 300, 0);
+	//glTranslatef(0, 17, 500);
+	//glRotatef((GLint)rotating, 0.0, 1.0, 0.0);
+	glRotatef((GLint)turning, 0.0, 1.0, 0.0);
+	glTranslatef(-400.0, 0.0, 0.0);
 	glCallList(display3);
 	glPopMatrix();
 	// box 3
@@ -159,7 +193,8 @@ void display(void) {
 	// end
 	// skybox
 	glPushMatrix();
-	glTranslatef(-1500, -1, -1500);
+	glTranslatef(-2000, -1, -2000);
+	glScalef(1.5, 1.5, 1.5);
 	glCallList(display5);
 	glPopMatrix();
 
@@ -167,7 +202,7 @@ void display(void) {
 	glPushMatrix();
 	glTranslatef(-200, 600, 0);
 	glScalef(10, 500, 10);
-	
+
 	glCallList(display6);
 	glPopMatrix();
 
@@ -212,12 +247,12 @@ void rotate_point(float angle) {
 // callback function for keyboard (alfanumeric keys)
 void callbackKeyboard(unsigned char key, int x, int y) {
 	switch (key) {
-		case 'w': case 'W':
-			camera_viewing_y += (10);
-			break;
-		case 's': case 'S':
-			camera_viewing_y -= (10);
-			break;
+	case 'w': case 'W':
+		camera_viewing_y += (10);
+		break;
+	case 's': case 'S':
+		camera_viewing_y -= (10);
+		break;
 	}
 }
 
@@ -228,10 +263,12 @@ void specialkeys(int key, int x, int y) {
 	if (key == GLUT_KEY_LEFT) {
 		total_moving_angle += -0.05;
 		rotate_point(-0.05);
-	} else if (key == GLUT_KEY_RIGHT) {
-		total_moving_angle += 0.05; 
+	}
+	else if (key == GLUT_KEY_RIGHT) {
+		total_moving_angle += 0.05;
 		rotate_point(0.05);
-	} else if (key == GLUT_KEY_DOWN) {
+	}
+	else if (key == GLUT_KEY_DOWN) {
 		//printf("Down key is pressed\n");
 		//camera_z += 10;
 		camera_x += (-10) * sin(total_moving_angle);//*0.1;
@@ -239,7 +276,8 @@ void specialkeys(int key, int x, int y) {
 		//camera_viewing_y -= 10;
 		camera_viewing_x += (-10) * sin(total_moving_angle);//*0.1;
 		camera_viewing_z += (-10) * -cos(total_moving_angle);//*0.1;
-	} else if (key == GLUT_KEY_UP) {
+	}
+	else if (key == GLUT_KEY_UP) {
 		//printf("Up key is pressed\n");
 		//camera_z -= 10;
 		camera_x += (10) * sin(total_moving_angle);//*0.1;
@@ -248,6 +286,113 @@ void specialkeys(int key, int x, int y) {
 		camera_viewing_z += (10) * -cos(total_moving_angle);//*0.1;
 		//camera_viewing_y += 10;
 	}
+}
+/*
+void orientMe(float ang) {
+
+	lx = sin(ang); lz = -cos(ang);
+
+}
+
+
+
+
+
+void moveMeFlat(int i) {
+
+	float newX = x + i * (lx)*0.5;
+
+	float newZ = z + i * (lz)*0.5;
+
+	if (z < -25 || z > 25) {
+		if (newX > -24 && newX < 24)
+			x = newX;
+	}
+	else {
+		if (newX > -99 && newX < 99)
+			x = newX;
+	}
+
+	if (x < -25 || x > 25) {
+		if (newZ > -24 && newZ < 24)
+			z = newZ;
+	}
+	else {
+		if (newZ > -99 && newZ < 99)
+			z = newZ;
+	}
+
+}
+
+void mouse(int button, int state, int x, int y) {
+	mouse_x = x;
+	mouse_y = y;
+	mouse_button = button;
+}
+
+//lx = sin(ang); lz = -cos(ang);
+// motion
+void motion(int x, int y) {
+	if (mouse_button == GLUT_LEFT_BUTTON) {
+
+		cameraAngle += (float(x - mouse_x) / width);
+
+		orientMe(cameraAngle);
+		ly -= (float(y - mouse_y) / height);
+	}
+	mouse_x = x;
+	mouse_y = y;
+	glutPostRedisplay();
+}
+
+void mySpecial(int key, int x, int y) {
+
+	switch (key) {
+
+	case GLUT_KEY_UP:
+		if (FAST)
+			moveMeFlat(2.0);
+		else
+			moveMeFlat(1.0);
+		break;
+
+	case GLUT_KEY_DOWN:
+		if (FAST)
+			moveMeFlat(-2.0);
+		else
+			moveMeFlat(-1.0);
+
+		break;
+
+	case GLUT_KEY_LEFT:
+
+		cameraAngle -= 0.04f;
+
+		orientMe(cameraAngle);
+
+		break;
+
+	case GLUT_KEY_RIGHT:
+
+		cameraAngle += 0.04f;
+
+		orientMe(cameraAngle);
+
+		break;
+
+	}
+
+	glutPostRedisplay();
+
+}
+*/
+void myIdle() {
+	if (o == 10) {
+		turning = (turning + 1) % 360;
+		o = 0;
+	}
+	o++;
+	glutPostRedisplay();
 }
 
 // main
@@ -259,8 +404,11 @@ int main(int argc, char* argv[]) {
 	glutCreateWindow("Sky Box");
 	glutReshapeFunc(reshape);
 	glutDisplayFunc(display);
-	glutIdleFunc(display);
-	
+	glutIdleFunc(myIdle);
+
+	//glutMotionFunc(motion);
+	//glutMouseFunc(mouse);
+
 	glutSpecialFunc(specialkeys);
 	glutKeyboardFunc(callbackKeyboard);
 	init();
